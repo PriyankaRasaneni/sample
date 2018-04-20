17e49b13f236ec8e21dcae02c1bc4eb6beba46a4
PriyankaRasaneni <priyankarasaneni@gmail.com>
2018-04-20 12:09:56 +0530

    added prepare function in config
    ccc

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
 exports.config = {
     framework: 'jasmine',
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['loginspec.js']
+    specs: ['loginspec.js'],
+     onPrepare: function() {
+        browser.manage().window().maximize();
+        browser.manage().timeouts().implicitlyWait(5000);
+     }
   }
